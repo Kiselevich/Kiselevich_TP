1cf58a79a4487f0da7d68099bf5eed89e494642c
Kiselevich <aleh.kiselevich@gmail.com>
2017-04-29 03:26:43 +0300

    Показ температуры из интернета

---
+++ Tempreature_Lab_06/Tempreature_Lab_06/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tempreature_Lab_06
+//
+//  Created by Admin on 21.04.17.
+//  Copyright © 2017 Oleg Kiselevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
